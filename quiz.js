10dcb0bccb7e7c5be65cba11bd334acc658490f6
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2017-12-24 13:57:42 +0000

    updated button
    attempt at converting from JSON

--- quiz.js
+++ quiz.js
@@ -1,22 +1,23 @@
-var assembleKey1 = function(parameters) {
-  var para_list = [];
+var assembleQuery1 = function(parameters) {
+  var query_string = [];
   for (var key in parameters) {
     if (parameters.hasOwnProperty(key)) {
-      var para_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
-      para_list.push(para_string);
+      var param_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
+      query_string.push(param_string);
     }
 
  }
-  return para_list.join("&");
+  return query_string.join("&");
 
 }
 
-   var url = "https://qriusity.com/v1/categories";
+var quizQuestion=function() {
+   var url = "https://opentdb.com/api.php";
    let params = {
-       limit: "20"
+       amount: "5"
      }
 
-   var query_url = url + "?" + assembleKey1(params);
+   var query_url = url + "?" + assembleQuery1(params);
      console.log(query_url);
 
    fetch(query_url)
@@ -28,39 +29,52 @@ var assembleKey1 = function(parameters) {
           console.log(results);
        });
 
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", query_url);
+xhttp.addEventListener("load", function() {
+  var response = JSON.parse(this.response);
+  console.log(response.results.question);
+})
+xhttp.send();
+
 // Supposed to call the quiz api when the button is clicked
-document.getElementById("button1").addEventListener("click", function() )
+ var button1 = document.getElementById("button1");
+ button1.addEventListener("click", function() {
+    console.log("clicked");
+  })
 
-       var assembleKey1 = function(parameters) {
-         var para_list = [];
-         for (var key in parameters) {
-           if (parameters.hasOwnProperty(key)) {
-             var para_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
-             para_list.push(para_string);
-           }
+}
 
-        }
-         return para_list.join("&");
+       // var assembleKey1 = function(parameters) {
+       //   var para_list = [];
+       //   for (var key in parameters) {
+       //     if (parameters.hasOwnProperty(key)) {
+       //       var para_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
+       //       para_list.push(para_string);
+       //     }
+       //
+       //  }
+       //   return para_list.join("&");
 
-       }
+       // }
 
-          var url = "https://qriusity.com/v1/categories/:id/questions";
-          params = {
-              id: "rand",
-              limit: "5"
-            }
-
-          var query_url = url + "?" + assembleKey1(params);
-            console.log(query_url);
-
-          fetch(query_url)
-              .then(function(response) {
-                 return response.json();
-              })
-              .then(function(data) {
-                 var results = data;
-                 console.log(results);
-              });
+          // var url = "https://qriusity.com/v1/categories/:id/questions";
+          // params = {
+          //     id: "rand",
+          //     limit: "5"
+          //   }
+          //
+          // var query_url = url + "?" + assembleKey1(params);
+          //   console.log(query_url);
+          //
+          // fetch(query_url)
+          //     .then(function(response) {
+          //        return response.json();
+          //     })
+          //     .then(function(data) {
+          //        var results = data;
+          //        console.log(results);
+          //     });
 
    query_url = "https://icanhazdadjoke.com/";
          console.log(query_url);
@@ -87,7 +101,7 @@ document.getElementById("button1").addEventListener("click", function() )
        filterPostsPerPage: "1"
    };
 
-   query_url = url + "?" + assembleKey1(params);
+   query_url = url + "?" + assembleQuery1(params);
    console.log(query_url);
 
    fetch(query_url)
