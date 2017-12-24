de8baae57bd64b7e75622d03035229feb647e75e
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2017-12-24 23:58:55 +0000

    take 800
    still stujmped

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
-var parseResponse = function() {
-  var response = JSON.parse(this.response);
-  console.log(response);
-}
+// var parseResponse = function() {
+//   var response = JSON.parse(this.response);
+//   console.log(response);
+// }
 
 var assembleQuery1 = function(parameters) {
    var query_string = [];
@@ -15,14 +15,14 @@ var assembleQuery1 = function(parameters) {
    return query_string.join("&");
  }
 
- var extractParameters = function() {
-   var query = window.location.hash.substring(1);
-   var params = query.split ("&");
+ // var extractParameters = function() {
+ //   var query = window.location.hash.substring(1);
+ //   var params = query.split ("&");
 
    var return_data = {};
-   for(var i = 0; i <params.length; i++) {
-       var keyvalue = params[i].split(",");
-       return_data[keyvalue[0]] = keyvalue[1];
+   for(var i = 0; i (params.length; i++) {
+       var question = results[0];
+       return_data[keyvalue[0]] = question["question"];
    }
    return return_data;
  }
@@ -58,19 +58,20 @@ var assembleQuery1 = function(parameters) {
            console.log(results);
         });
 
-var quizQuestion=function() {
- var xhttp = new XMLHttpRequest();
- xhttp.addEventListener("load", parseResponse());
- xhttp.open("GET", url);
- xhttp.send();
-    console.log(response);
-    processResponse(response);
- }
-
+// var quizQuestion=function() {
+//  var xhttp = new XMLHttpRequest();
+//  xhttp.addEventListener("load", parseResponse());
+//  xhttp.open("GET", url);
+//  xhttp.send();
+//     console.log(response);
+//     processResponse(response);
+//  }
+//
+// if
 
 // Supposed to call the quiz api when the button is clicked
-  // var button1 = document.getElementById("button1");
-  // button1.addEventListener("click", quizQuestion);
+   var button1 = document.getElementById("button1");
+   button1.addEventListener("click", function());
 
 
         // var assembleKey1 = function(parameters) {
@@ -86,41 +87,23 @@ var quizQuestion=function() {
 
        // }
 
-          // var url = "https://qriusity.com/v1/categories/:id/questions";
-          // params = {
-          //     id: "rand",
-          //     limit: "5"
-          //   }
-          //
-          // var query_url = url + "?" + assembleKey1(params);
-          //   console.log(query_url);
-          //
-          // fetch(query_url)
-          //     .then(function(response) {
-          //        return response.json();
-          //     })
-          //     .then(function(data) {
-          //        var results = data;
-          //        console.log(results);
-          //     });
+    query_url = "https://icanhazdadjoke.com/";
+          console.log(query_url);
+    var request = new Request(query_url, {
+          headers: new Headers({
+              'Accept': 'text/plain'
+          })
+    });
 
-   // query_url = "https://icanhazdadjoke.com/";
-   //       console.log(query_url);
-   // var request = new Request(query_url, {
-   //       headers: new Headers({
-   //           'Accept': 'text/plain'
-   //       })
-   // });
-   //
-   // fetch(request)
-   //     .then(function(response) {
-   //       return response.text();
-   //     })
-   //     .then(function(data) {
-   //       var results = data;
-   //       console.log(results);
-   //
-   //     });
+    fetch(request)
+        .then(function(response) {
+          return response.text();
+        })
+        .then(function(data) {
+          var results = data;
+          console.log(results);
+
+        });
 
    // The link as been altered so that the error was bypassed
    // url = "https://cors-anywhere.herokuapp.com/http://quotesondesign.com/wp-json/posts";
