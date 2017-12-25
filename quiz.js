be394711b83215a418f53d09dfa80b5afc75effd
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2017-12-25 15:16:03 +0000

    PLS WRK
    PLS WRK

--- quiz.js
+++ quiz.js
@@ -3,24 +3,86 @@
 //   console.log(response);
 // }
 
+// Defines how the url will be constructed
 var assembleQuery1 = function(parameters) {
-   var query_string = [];
+  var query_string = [];
   for (var key in parameters) {
-     if (parameters.hasOwnProperty(key)) {
-       var param_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
-       query_string.push(param_string);
-     }
+    if (parameters.hasOwnProperty(key)) {
+      var param_string = encodeURIComponent(key) + "=" + encodeURIComponent(parameters[key]);
+      query_string.push(param_string);
+    }
 
   }
-   return query_string.join("&");
- }
+  // Calls to construct url
+  return query_string.join("&");
+}
 
- // var extractParameters = function() {
- //   var query = window.location.hash.substring(1);
- //   var params = query.split ("&");
+
+ function shuffle(a) {
+	 var counter;
+	 for (var counter = a.length; counter > 0;) {
+		 var target = Math.floor(Math.random() * counter);
+		 counter--;
+		 var nextOne = a[counter];
+		 a[counter] = a[target];
+		 a[target] = nextOne;
+    }
+}
+
+// when the quiz starts it will start at
+// index -1 and there will be 0 correct answer
+var questions;
+var state = {
+	nextQuestion: -1,
+	correctAnswers: 0
+}
+
+// This function makes sure that the quiz continues
+// until the question limit is reached (5).
+// Then the quiz will end
+function NextQuestion()
+{
+	state.nextQuestion++;
+	if (state.nextQuestion >= questions.length)
+	{
+      document.getElementById("button1").input = value="SUBMIT";
+      // document.getElementById("button1")
+
+    //done
+    // set questions to -1
+    // document.navigate where I will be able to tell how many correct questions there are
+    // sessionid attach it to every request
+	}
+	else
+	{
+		if (state.nextQuestion === 0)
+		{
+			state.correctAnswers = 0;
+		}
+
+		document.getElementById("question").innerHTML = questions[state.nextQuestion].question;
+		var radioHtml = '';
+
+		for (var i in questions[state.nextQuestion].answers)
+		{
+			radioHtml += '<input type="radio" name="answerValue" value="' + i + '"';
+			if ( i == 0 ) {
+				radioHtml += ' checked="checked"';
+			}
+			radioHtml += '/><label>' + questions[state.nextQuestion].answers[i].value +'</label><br/>';
+		}
+		console.log(radioHtml);
+		document.getElementById("answers").innerHTML = radioHtml;
+	}
+
+}
+
+  var extractParameters = function() {
+    var query = window.location.hash.substring(1);
+    var params = query.split ("&");
 
    var return_data = {};
-   for(var i = 0; i (params.length; i++) {
+   for(var i = 0; i <params.length; i++) {
        var question = results[0];
        return_data[keyvalue[0]] = question["question"];
    }
@@ -40,7 +102,6 @@ var assembleQuery1 = function(parameters) {
 
 
 // Code to assemble api link request correctly
-
    var url = "https://opentdb.com/api.php";
     let params = {
         amount: "5"
@@ -53,10 +114,32 @@ var assembleQuery1 = function(parameters) {
         .then(function(response) {
            return response.json();
         })
+        // Creates a new function to store an array of questions
         .then(function(data) {
-           var results = data;
-           console.log(results);
+			questions = new Array();
+			for (var i in data.results)
+			{
+        // Grabs a question
+        var question = data.results[i];
+				var q = new Array();
+				for (var j in question.incorrect_answers)
+				{
+          // pushes incorrect answers into a new array
+          q.push ({correct: 0, value: question.incorrect_answers[j]});
+				}
+				q.push({correct: 1, value: question.correct_answer});
+        // shuffles answers so correct answer isn't always in the same place
+        shuffle(q);
+        // after shuffling push into an object with a question and an array of answers
+				questions.push({question: question.question, answers: q});
+			}
+            console.log(data);
+            shuffle(questions);
+		    console.log(questions);
+		   	NextQuestion();
         });
+
+
 
 // var quizQuestion=function() {
 //  var xhttp = new XMLHttpRequest();
@@ -69,9 +152,28 @@ var assembleQuery1 = function(parameters) {
 //
 // if
 
+function checkResult()
+{
+	var answers = document.getElementsByName("answerValue");
+	var selectedAnswer;
+
+	for(var i in answers) {
+		if(answers[i].checked) {
+			selectedAnswer = answers[i].value;
+		}
+	}
+
+	if (questions[state.nextQuestion].answers[selectedAnswer].correct)
+	{
+		state.correctAnswers++;
+	}
+
+	NextQuestion();
+ }
+
 // Supposed to call the quiz api when the button is clicked
    var button1 = document.getElementById("button1");
-   button1.addEventListener("click", function());
+   button1.addEventListener("click", checkResult);
 
 
         // var assembleKey1 = function(parameters) {
